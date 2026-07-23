5e449f19158536930105f1f1d9ba9387d7af90d2
stepansedrakyan14-svg <stepansedrakyan14@gmail.com>
2026-07-23 18:37:47 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,32 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension de Naturalisation installée !");
+    console.log("Extension Naturalisation installée.");
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "STATUS") {
-    console.log("Statut :", message.data);
-  }
+
+    if (message.type === "STATUS_UPDATE") {
+
+        chrome.storage.local.get(["lastStatus"], (result) => {
+
+            const oldStatus = result.lastStatus;
+
+            if (oldStatus !== message.status) {
+
+                chrome.storage.local.set({
+                    lastStatus: message.status
+                });
+
+                chrome.notifications.create({
+                    type: "basic",
+                    iconUrl: "icons/icon128.png",
+                    title: "Naturalisation",
+                    message: "Nouveau statut : " + message.status
+                });
+
+            }
+
+        });
+
+    }
+
 });
